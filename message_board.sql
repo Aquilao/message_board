4e1b29e220c52135d6becb81b8764fc71cac13c2
Aquilao <aquilao@outlook.com>
2018-06-19 08:31:28 +0800

    [^] 修改数据库结构

--- message_board.sql
+++ message_board.sql
@@ -24,15 +24,25 @@ DROP TABLE IF EXISTS `messages`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
   `mid` int(5) NOT NULL AUTO_INCREMENT,
-  `author` varchar(20) NOT NULL,
-  `target` varchar(20) NOT NULL,
-  `title` varchar(200) NOT NULL,
-  `message` varchar(1000) NOT NULL,
+  `author` varchar(10) DEFAULT NULL,
+  `target` varchar(10) DEFAULT NULL,
+  `title` varchar(18) DEFAULT NULL,
+  `message` varchar(200) DEFAULT NULL,
   `time` datetime NOT NULL,
   `reply` int(5) DEFAULT NULL,
   PRIMARY KEY (`mid`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'admin','test','message test','Hello world!','2018-06-12 09:41:50',NULL),(2,'Aquilao','test','HELLO','How Are You.','2018-06-12 14:11:56',NULL),(3,'test','Aquilao','send message te','ä½ å¥½å•Š','2018-06-12 17:15:12',NULL),(4,'Aquilao','test','lol','let us play lol!','2018-06-12 22:01:51',NULL),(6,'test','Aquilao','FUCK','Hey men! fuck you!','2018-06-12 23:45:14',NULL),(7,'test','Aquilao','HI','I am fine.Thank you,and you?','2018-06-14 15:18:42',2),(9,'Aquilao','test','Re:HI','OK','2018-06-14 15:40:58',7),(10,'Aquilao','admin','hi','you are admin?','2018-06-15 14:05:38',NULL),(15,'admin','Aquilao','reply','lol','2018-06-15 20:16:14',12),(16,'Aquilao','admin','Re:reply','<script>alert(/xss/)</script>','2018-06-19 08:20:28',15);
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -43,11 +53,21 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `uid` int(4) NOT NULL AUTO_INCREMENT,
-  `user` varchar(20) NOT NULL,
-  `pwd` varchar(20) NOT NULL,
+  `user` varchar(10) DEFAULT NULL,
+  `pwd` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin'),(2,'test','test'),(3,'Aquilao','123456'),(4,'ihexo','ihexo'),(5,'Agnes','root'),(6,'php','python');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +78,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-13 23:26:22
+-- Dump completed on 2018-06-19  8:30:08
