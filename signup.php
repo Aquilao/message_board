b747198281bb2d436d71434ebe3463bbc9dc3ed4
Aquilao <aquilao@outlook.com>
2018-11-21 15:11:14 +0800

    [^] Fix bug about can't sign up

--- signup.php
+++ signup.php
@@ -9,17 +9,19 @@ if(isset($_POST['user']{3}) && !isset($_POST['user']{10}) && isset($_POST['pwd']
   $select_user = "select user from users where user = '$user'";
   if ($result = mysqli_query($db, $select_user)) {                              # 判断是否重名
     $row = mysqli_fetch_assoc($result);
-    echo "<script>alert('该用户名已被使用')</script>";
-  }
-  else {
-    $insert = "insert into users value(null, '$user', '$pwd')";                 # 注册用户
-    if (mysqli_query($db, $insert)) {
-      echo "<script>alert('注册成功！');location.href='login.php'</script>";
+    if ($row != NULL) {
+      echo "<script>alert('该用户名已被使用')</script>";
     }
     else {
-      echo "<script>alert('注册失败！')</script>";
+      $insert = "insert into users value(null, '$user', '$pwd')";                 # 注册用户
+      if (mysqli_query($db, $insert)) {
+        echo "<script>alert('注册成功！');location.href='login.php'</script>";
+      }
+      else {
+        echo "<script>alert('注册失败！')</script>";
+      }
+      mysqli_close($db);
     }
-    mysqli_close($db);
   }
 }
 ?>
@@ -33,7 +35,7 @@ if(isset($_POST['user']{3}) && !isset($_POST['user']{10}) && isset($_POST['pwd']
 <body>
   <div class="block">
   <h1>Sign Up</h1>
-  <form action="./signup.php" method="post">
+  <form action="signup.php" method="post">
     <div> <input type="text" name="user" maxlength="10" placeholder="username"/></div>
     <div> <input type="password" name="pwd" maxlength="10" placeholder="password"/></div>
     <div> <button type="submit">sign up</button></div>
